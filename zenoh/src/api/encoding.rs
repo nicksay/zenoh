3bbe4e74ff9f88c6392964f84cca2d4c0dcca797
Alex Nicksay <nicksay@gmail.com>
2025-06-12 07:38:53 -0700

    Include Avro as an application-specific encoding.

--- zenoh/src/api/encoding.rs
+++ zenoh/src/api/encoding.rs
@@ -462,6 +462,13 @@ impl Encoding {
         id: 52,
         schema: None,
     });
+    /// Application-specific [Avro](https://avro.apache.org/)-encoded data.
+    ///
+    /// Constant alias for string: `"application/avro"`.
+    pub const APPLICATION_AVRO: Encoding = Self(zenoh_protocol::core::Encoding {
+        id: 53,
+        schema: None,
+    });
 
     const ID_TO_STR: phf::Map<EncodingId, &'static str> = phf_map! {
         0u16 => "zenoh/bytes",
@@ -517,6 +524,7 @@ impl Encoding {
         50u16 => "video/raw",
         51u16 => "video/vp8",
         52u16 => "video/vp9",
+        53u16 => "application/avro",
         // The 0xFFFFu16 is used to indicate an custom encoding where both encoding and schema
         // are stored in the schema field.
         0xFFFFu16 => "",
@@ -576,6 +584,7 @@ impl Encoding {
         "video/raw" => 50u16,
         "video/vp8" => 51u16,
         "video/vp9" => 52u16,
+        "application/avro" => 53u16,
     };
 
     /// The default [`Encoding`] is [`ZENOH_BYTES`](Encoding::ZENOH_BYTES).
